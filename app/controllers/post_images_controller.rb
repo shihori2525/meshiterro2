f4168862bb019788dce384e78001eb3f44a5b524
shihori2525 <rirakkuma.250@gmail.com>
2022-03-12 22:48:12 +0900

    [Add]post images controller

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,19 @@
+class PostImagesController < ApplicationController
+  def def new
+    @ = .new
+  end
+  def create
+
+  end
+
+  def index
+
+  end
+  def show
+
+  end
+
+  def destroy
+    
+  end
+end
