8b4e320357108e3dac192b11e6f489a9d0c1a61b
shihori2525 <rirakkuma.250@gmail.com>
2022-03-13 21:30:45 +0900

    [Add]post

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,12 +1,16 @@
 class PostImagesController < ApplicationController
   def def new
-    @ = .new
+    @post_image = PostImage.new
   end
   def create
-
+    @post_image = PostImage.new
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
+    @post_images = PostImage.all
 
   end
   def show
@@ -14,6 +18,11 @@ class PostImagesController < ApplicationController
   end
 
   def destroy
-    
+
+  end
+
+  private
+  def post_image_params
+    params.require(:post_image).permit(:shop_name,:image,:caption)
   end
 end
