5b2eb2fb1a682773357b3586c3200eb0d6c24b20
shihori2525 <rirakkuma.250@gmail.com>
2022-03-16 21:12:58 +0900

    [Fix]index

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,7 +3,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
   def create
-    @post_image = PostImage.new
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
@@ -25,7 +25,9 @@ class PostImagesController < ApplicationController
   end
 
   private
+  
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5ac0d23f761d322532d5c251b7d9f4ccc50bb45f394c5f4f177b97274f1b9a72ecefe5bcbf75084b2f863086889a66c1d83dd41f999bb4a6d6d710c8a274ee03'
