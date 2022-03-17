ccc30cecaa96dceba6b8eaa1a7d667ffad000157
shihori2525 <rirakkuma.250@gmail.com>
2022-03-17 21:23:53 +0900

    [Add]post comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_142742) do
+ActiveRecord::Schema.define(version: 2022_03_17_122309) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
