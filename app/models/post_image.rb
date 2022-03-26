293e928ffe93faf82dc0801641a3235f479a134d
shihori2525 <rirakkuma.250@gmail.com>
2022-03-26 22:43:15 +0900

    [Fix]favorite

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -6,6 +6,6 @@ class PostImage < ApplicationRecord
   has_many :favorites,dependent: :destroy
 
   def favorited_by?(user)
-    favorites.where(user_id:user.id).exists?
+    favorites.where(user_id: user.id).exists?
   end
 end
