d9e1abc5d9e08c8c0e1374ae69d31ebd5a61c009
shihori2525 <rirakkuma.250@gmail.com>
2022-03-31 22:52:50 +0900

    [Add]validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!,except:[:top]
   before_action :configure_permiteed_parameters,if: :devise_controller?
 
   def after_sigin_in_path_for(resource)
